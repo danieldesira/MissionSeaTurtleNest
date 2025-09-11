3bdd211d33c0745e1d8865e9a728e96333d782ac
Daniel Desira <desiradaniel2007@gmail.com>
2025-09-11 19:17:21 +0200

    Turtle-5: simplify subscription post

--- src/serviceWorkers/notificationServiceWorker.ts
+++ src/serviceWorkers/notificationServiceWorker.ts
@@ -2,35 +2,21 @@
 
 const swSelf = self as unknown as ServiceWorkerGlobalScope;
 
-interface SaveSubPayload {
-  endpoint: string;
-  p256dh: string;
-  auth: string;
-}
-
 const apiBaseUrl =
   location.hostname === "localhost"
     ? "https://localhost:32769"
     : "https://tochange.com";
 
-const savePushSubscription = async (subscription: SaveSubPayload) => {
+const savePushSubscription = async (subscription: PushSubscription) => {
   const res = await fetch(`${apiBaseUrl}/api/subscription`, {
     method: "POST",
     headers: {
       "Accept-Content": "application/json",
+      "Content-Type": "application/json",
     },
     body: JSON.stringify(subscription),
   });
   return await res.json();
-};
-
-const convertArrayBufferToString = (buffer: ArrayBuffer | null) => {
-  if (!buffer) {
-    return "";
-  }
-
-  const decoder = new TextDecoder("utf-8");
-  return decoder.decode(buffer);
 };
 
 swSelf.addEventListener("activate", async () => {
@@ -39,11 +25,7 @@ swSelf.addEventListener("activate", async () => {
     applicationServerKey:
       "BDRd50z5ESS32mFqaLVJIT-Ap-EgwELoHWo2BFa5_oZSGHKLtQcM3w4-nEA2XOnM6QMN1zYApbs4SjjQgt56rQk",
   });
-  await savePushSubscription({
-    endpoint: subscription.endpoint,
-    p256dh: convertArrayBufferToString(subscription.getKey("p256dh")),
-    auth: convertArrayBufferToString(subscription.getKey("auth")),
-  });
+  await savePushSubscription(subscription);
 });
 
 swSelf.addEventListener("push", () => {
