8d7bdf360e6149a0fab101cef2d183d8b8c25ebf
Daniel Desira <desiradaniel2007@gmail.com>
2026-09-20 23:38:42 +0200

    #122: implement selection ui

--- src/utils/ui/avatarField.ts
+++ src/utils/ui/avatarField.ts
@@ -2,13 +2,68 @@ import PrettyDialog from "../../webComponents/dialog/PrettyDialog";
 import PrettyButton from "../../webComponents/form/PrettyButton";
 import { $id } from "./domQuery";
 
+const AVATAR_MAX_WIDTH = 150;
+const AVATAR_MAX_HEIGHT = 150;
+
+type AvatarSquareSelection = {
+  x: number;
+  y: number;
+};
+
+type AvatarSquareCoordinates = {
+  x1: number;
+  y1: number;
+  x2: number;
+  y2: number;
+};
+
+const getAvatarSquareCoordinates = (
+  selection: AvatarSquareSelection,
+): AvatarSquareCoordinates => {
+  const x1 = selection.x - AVATAR_MAX_WIDTH / 2;
+  const y1 = selection.y - AVATAR_MAX_HEIGHT / 2;
+  const x2 = x1 + AVATAR_MAX_WIDTH / 2;
+  const y2 = y1 + AVATAR_MAX_HEIGHT / 2;
+  return { x1, y1, x2, y2 };
+};
+
+const canvas = $id("avatarPreviewCanvas") as HTMLCanvasElement;
+
+const selection: AvatarSquareSelection = {
+  x: AVATAR_MAX_WIDTH / 2,
+  y: AVATAR_MAX_HEIGHT / 2,
+};
+
+const image = new Image();
+
+image.onload = () => {
+  canvas.width = image.width;
+  canvas.height = image.height;
+  const ctx = canvas.getContext("2d");
+  ctx?.drawImage(image, 0, 0);
+
+  const context = canvas.getContext("2d");
+
+  if (context) {
+    paintSelectionSquare(context, selection);
+  }
+};
+
+const paintSelectionSquare = (
+  context: CanvasRenderingContext2D,
+  selection: AvatarSquareSelection,
+) => {
+  const { x1, y1 } = getAvatarSquareCoordinates(selection);
+  context.strokeStyle = "pink";
+  context.lineWidth = 3;
+  context.strokeRect(x1, y1, AVATAR_MAX_WIDTH, AVATAR_MAX_HEIGHT);
+};
+
 export const setupAvatarField = () => {
   const avatarBtn = $id("avatarBtn") as PrettyButton;
   const fileInput = $id("avatarInput") as HTMLInputElement;
 
-  avatarBtn.on("click", () => {
-    fileInput.click();
-  });
+  avatarBtn.on("click", () => fileInput.click());
 
   fileInput.addEventListener("change", (event: Event) => {
     const target = event.target as HTMLInputElement;
@@ -19,20 +74,23 @@ export const setupAvatarField = () => {
       dialog.open();
     }
 
-    const canvas = $id("avatarPreviewCanvas") as HTMLCanvasElement;
     if (canvas && selectedFile) {
       const reader = new FileReader();
       reader.onload = (e) => {
-        const img = new Image();
-        img.onload = () => {
-          canvas.width = img.width;
-          canvas.height = img.height;
-          const ctx = canvas.getContext("2d");
-          ctx?.drawImage(img, 0, 0);
-        };
-        img.src = e.target?.result as string;
+        image.src = e.target?.result as string;
       };
       reader.readAsDataURL(selectedFile);
+    }
+  });
+
+  canvas.addEventListener("click", (event: MouseEvent) => {
+    const rect = canvas.getBoundingClientRect();
+    selection.x = event.clientX - rect.left;
+    selection.y = event.clientY - rect.top;
+    const context = canvas.getContext("2d");
+    if (context) {
+      context.drawImage(image, 0, 0);
+      paintSelectionSquare(context, selection);
     }
   });
 };
